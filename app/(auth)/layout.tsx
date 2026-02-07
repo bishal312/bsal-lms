f978b87856b832052d82cb16fa38d8a2d713e8ce
Bsal <bishalm626@gmail.com>
2026-02-07 20:21:14 +0545

    feat: implement authentication with better-auth and Prisma integration
    - Removed old logo image.
    - Added VSCode settings for Prisma.
    - Created authentication route using better-auth.
    - Implemented auth client for better-auth.
    - Set up authentication logic with Prisma adapter.
    - Established database connection using PostgreSQL and Prisma.
    - Defined environment variables for database and authentication.
    - Configured Prisma schema with User, Session, Account, and Verification models.
    - Added new logo image.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,8 +18,8 @@ export default function authLayout({ children }: { children: ReactNode }) {
         Back
       </Link>
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link className="flex items-center gap-2 self-center font-medium" href={"/"}>
-          <Image src={"/logo.jpg"} alt="logo" width={120} height={120} />
+        <Link className="flex items-center gap-2 self-center font-medium justify-center mx-auto" href={"/"}>
+          <Image className="pointer-events-none" src={"/logo-bsal.jpg"} alt="logo" width={80} height={80} />
           BsalLMS
         </Link>
         {children}
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,40 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { Loader } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 
-export default function loginPage() {
+export default function LoginPage() {
+
+  const [githubPending, startGitHubTransition] = useTransition();
+
+  function Github() {
+    return <SiGithub color='#ffffff' size={24} />;
+  }
+
+  async function signInWithGithub() {
+    startGitHubTransition(async () => {
+      await authClient.signIn.social({
+        provider: 'github',
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirectede...");
+          },
+          onError: () => {
+            toast.error("Internal Server error!")
+          }
+        }
+      })
+    })
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -13,9 +42,18 @@ export default function loginPage() {
         <CardDescription>Login with your github/gmail Account</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
-        <Button className="w-full" variant={"outline"}>
-          <GithubIcon className="size-4" />
-          Login with github Account
+        <Button disabled={githubPending} onClick={signInWithGithub} className="w-full" variant={"outline"}>
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              {Github()}
+              Login with github Account
+            </>
+          )}
         </Button>
 
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0
